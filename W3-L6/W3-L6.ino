9d3a843f45cc493bc6eaa29f9dcd94b42de9b625
ConnorCarlson <40050816+ConnorCarlson@users.noreply.github.com>
2021-04-20 02:41:04 -0700

    Update W3-L6.ino

--- W3-L6/W3-L6.ino
+++ W3-L6/W3-L6.ino
@@ -1,60 +1,39 @@
-const int RGB_RED_PIN = 3;
-const int RGB_GREEN_PIN  = 5;
-const int RGB_BLUE_PIN  = 6;
-const int DELAY_MS = 20;
-const int MAX_COLOR_VALUE = 255;
+const int DELAY_MS = 1000;       // delay between color changes in ms
+const int RGB_RED_LED_PIN = 6;   // indicated by orange wire
+const int RGB_GREEN_LED_PIN = 5; // indicated by green wire
+const int RGB_BLUE_LED_PIN = 3;  // indicated by blue wire
 
-enum RGB{
-  RED,
-  GREEN,
-  BLUE,
-  NUM_COLORS
-};
-
-int _rgbLedValues[] = {255, 0, 0}; // Red, Green, Blue
-enum RGB _curFadingUpColor = GREEN;
-enum RGB _curFadingDownColor = RED;
-const int FADE_STEP = 5;  
-
-void setup() {
-  pinMode(RGB_RED_PIN, OUTPUT);
-  pinMode(RGB_GREEN_PIN, OUTPUT);
-  pinMode(RGB_BLUE_PIN, OUTPUT);
-  Serial.begin(9600); 
-  Serial.println("Red, Green, Blue");
-
-  setColor(_rgbLedValues[RED], _rgbLedValues[GREEN], _rgbLedValues[BLUE]);
-  delay(DELAY_MS);
-}
-
-void loop() {
-
-  _rgbLedValues[_curFadingUpColor] += FADE_STEP;
-  _rgbLedValues[_curFadingDownColor] -= FADE_STEP;
-
-  if(_rgbLedValues[_curFadingUpColor] > MAX_COLOR_VALUE){
-    _rgbLedValues[_curFadingUpColor] = MAX_COLOR_VALUE;
-    _curFadingUpColor = (RGB)(((int)_curFadingUpColor + 1) % NUM_COLORS);
-
-  }
-
-  if(_rgbLedValues[_curFadingDownColor] < 0){
-    _rgbLedValues[_curFadingDownColor] = 0;
-    _curFadingDownColor = (RGB)(((int)_curFadingDownColor + 1) % NUM_COLORS);
-  }
-
-  setColor(_rgbLedValues[RED], _rgbLedValues[GREEN], _rgbLedValues[BLUE]);
-  delay(DELAY_MS);
-}
-
-void setColor(int red, int green, int blue)
+void setup()
 {
-  Serial.print(red);
-  Serial.print(", ");
-  Serial.print(green);
-  Serial.print(", ");
-  Serial.println(blue);
-  analogWrite(RGB_RED_PIN, red);
-  analogWrite(RGB_GREEN_PIN, green);
-  analogWrite(RGB_BLUE_PIN, blue);  
+  // Set the red, green, and blue RGB LED pins to output
+  pinMode(RGB_RED_LED_PIN, OUTPUT);
+  pinMode(RGB_BLUE_LED_PIN, OUTPUT);
+  pinMode(RGB_GREEN_LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+  // red
+  setRgbLedColor(HIGH, LOW, LOW);
+  delay(DELAY_MS);
+  
+  // green
+  setRgbLedColor(LOW, HIGH, LOW);
+  delay(DELAY_MS);
+  
+  // blue
+  setRgbLedColor(LOW, LOW, HIGH);
+  delay(DELAY_MS);
+  
+  // purple
+  setRgbLedColor(HIGH, LOW, HIGH);
+  delay(DELAY_MS);
+  
+  // turqoise
+  setRgbLedColor(LOW, HIGH, HIGH);
+  delay(DELAY_MS);
+  
+  // white
+  setRgbLedColor(HIGH, HIGH, HIGH);
+  delay(DELAY_MS);
 }
