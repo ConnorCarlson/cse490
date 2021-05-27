9b323239aab610aca9758f9c887400a73ea2fa02
ConnorCarlson <connor20c@gmail.com>
2021-05-27 01:01:36 -0700

    adjust

--- P5_serial_lesson/P5_serial_lesson.ino
+++ P5_serial_lesson/P5_serial_lesson.ino
@@ -1,12 +1,4 @@
-/**
- * Reads in an analog value and sends a normalized value [0, 1] inclusive
- * over the serial port with 4 decimal point precision.
- * 
- * By Jon E. Froehlich
- * @jonfroehlich
- * http://makeabilitylab.io
- * 
- */
+
 #include <math.h> 
 #include <Adafruit_LIS3DH.h>
 const int DELAY_MS = 5;
@@ -21,11 +13,9 @@ void loop() {
   acc.read();
   
 
-  /* Or....get a new sensor event, normalized */
   sensors_event_t event;
   acc.getEvent(&event);
 
-  /* Display the results (acceleration is measured in m/s^2) */
   float accX = event.acceleration.x;
   float accY = event.acceleration.y;
   float accZ = event.acceleration.z;
